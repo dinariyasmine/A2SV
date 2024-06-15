9c04f086d19be059841ba734a70b4aff300a6d5a
Yasmine Dinari <ly_dinari@esi.dz>
2024-06-15 23:27:52 +0100

    Created ' King Escape' by A2SV Hub

---
+++ 15-Jun-2024/ King Escape 168802.py
@@ -0,0 +1,15 @@
+# Problem:  King Escape - https://codeforces.com/problemset/problem/1033/A
+
+a = int(input())
+ax, ay = map(int, input().split())
+bx, by = map(int, input().split())
+cx, cy = map(int, input().split())
+
+ssx = (bx < ax) == (cx < ax)
+ssy = (by < ay) == (cy < ay)
+
+if ssx and ssy:
+    print("YES")
+else:
+    
+    print("NO")
