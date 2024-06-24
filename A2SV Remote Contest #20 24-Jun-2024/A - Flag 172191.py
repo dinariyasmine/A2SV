f10cc73999fa15a3c35fbf44c6dbd69adbbb373d
Yasmine Dinari <ly_dinari@esi.dz>
2024-06-24 18:54:22 +0100

    Created 'A - Flag' by A2SV Hub

---
+++ A2SV Remote Contest #20 24-Jun-2024/A - Flag 172191.py
@@ -0,0 +1,29 @@
+# Problem: A - Flag - https://codeforces.com/gym/531416/problem/A
+
+def row_vrf(row):
+    ref = row[0]
+    for j in range(len(row)):
+        if row[j]!= ref:
+            return False      
+    return True
+n, m=map(int,input().split())
+mtx =[]
+x = True
+used= set()
+
+for i in range(n):
+    row= list(input().strip())
+    if not row_vrf(row):
+        print('NO')
+        x = False
+        break
+    mtx.append(row)
+if x:
+    w =True
+    for k in range(1, n):
+        if mtx[k][0]== mtx[k-1][0]:
+            print('NO')
+            w= False
+            break
+    if w:
+        print('YES')
