5e16bb31d9920f84ef5046e2d27d7b2304571b75
Yasmine Dinari <ly_dinari@esi.dz>
2024-06-29 22:43:24 +0100

    Created 'A - Forever Winter' by A2SV Hub

---
+++ A2SV Remote Contest #21 29-Jun-2024/A - Forever Winter 174800.py
@@ -0,0 +1,34 @@
+# Problem: A - Forever Winter - https://codeforces.com/gym/532332/problem/A
+
+import collections
+t = int(input())
+for _ in range(t):
+    n, m = map(int,input().split())
+    adj_lst = {i: [] for i in range(1, n + 1)}
+    # n => nbr of nodes
+    # m => nbr of edges
+    for _ in range(m):
+        u, v = map(int,input().split())
+        adj_lst[u].append(v)
+        adj_lst[v].append(u)
+    
+    leafs = set()
+    for i in range(1,n+1):
+        if len(adj_lst[i])==1:
+            leafs.add(i)
+            
+            
+        
+    nbr_leafs = len(leafs)
+    
+    
+    
+    x = n - nbr_leafs - 1
+    
+    y = nbr_leafs // x
+    
+    print(x,y)
+    
+        
+    
+    
