8f70fc0b43905b4ee887626ba5238edc592ebbf4
Yasmine Dinari <ly_dinari@esi.dz>
2024-06-29 22:43:20 +0100

    Created 'B - New Year Transportation' by A2SV Hub

---
+++ A2SV Remote Contest #21 29-Jun-2024/B - New Year Transportation 174799.py
@@ -0,0 +1,18 @@
+# Problem: B - New Year Transportation - https://codeforces.com/gym/532332/problem/B
+
+n,t = map(int, input().split())
+a = list(map(int, input().split()))
+
+#changed to iterative maybe recusion was too deep
+curr = 1 
+
+while curr < t:
+    
+    curr =curr +a[curr-1]
+    if curr == t:
+        print("YES")
+        break
+else:
+    
+    
+    print("NO")
