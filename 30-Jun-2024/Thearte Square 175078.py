259f7f8b4861784e7c486f26c6d88e539d63e3d5
Yasmine Dinari <ly_dinari@esi.dz>
2024-06-30 22:51:16 +0100

    Created 'Thearte Square' by A2SV Hub

---
+++ 30-Jun-2024/Thearte Square 175078.py
@@ -0,0 +1,12 @@
+# Problem: Thearte Square - https://codeforces.com/problemset/problem/1/A
+
+import math
+
+
+
+def minF(n,m,a):
+    L= (n+a-1) //a
+    W= (m+a-1)// a
+    return L*W
+n,m,a = map(int, input().split())
+print(minF(n, m, a))
