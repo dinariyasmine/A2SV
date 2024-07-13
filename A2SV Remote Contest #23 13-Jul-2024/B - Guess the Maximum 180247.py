c6ebdc47f2c614ce92addfaee870f9bba70a77b6
Yasmine Dinari <ly_dinari@esi.dz>
2024-07-13 23:41:34 +0100

    Created 'B - Guess the Maximum' by A2SV Hub

---
+++ A2SV Remote Contest #23 13-Jul-2024/B - Guess the Maximum 180247.py
@@ -0,0 +1,17 @@
+# Problem: B - Guess the Maximum - https://codeforces.com/gym/535309/problem/B
+
+t = int(input().strip())
+
+res = []
+for _ in range(t):
+    n = int(input().strip())
+    s = list(map(int,input().strip().split()))
+    
+    mini = max(s[0],s[1])
+    
+    for i in range(1, n - 1):
+        mini = min(mini,max(s[i],s[i + 1]))
+    
+    res.append(str(mini-1))
+
+print("\n".join(res))
