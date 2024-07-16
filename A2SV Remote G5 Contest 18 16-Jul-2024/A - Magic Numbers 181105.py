eb4bbd08bc89d220797af94e982f1661bf3c72a4
Yasmine Dinari <ly_dinari@esi.dz>
2024-07-17 00:44:09 +0100

    Created 'A - Magic Numbers' by A2SV Hub

---
+++ A2SV Remote G5 Contest 18 16-Jul-2024/A - Magic Numbers 181105.py
@@ -0,0 +1,41 @@
+# Problem: A - Magic Numbers - https://codeforces.com/gym/528793/problem/A
+
+
+def LI():return list(map(int, input()))
+def LSI():return input().split(" ")
+def LC():return input().split()
+def I():return int(input())
+def S(): return input()
+
+
+
+t = S()
+
+authorized = {'1','4'}
+failed = False
+coherent = False
+for digit in t:
+    if digit not in authorized:
+        print('NO')
+        failed = True
+        break
+
+if not failed:
+    for i in range(len(t)):
+        if (t[i]) == '4':
+            if i == 0 :
+                print('NO')
+                coherent = True
+                break
+            if not coherent:
+                if not((t[i-1])=='1' or (t[i-1])=='4' and (t[i-2]) == '1'):
+                    coherent = True
+                    print('NO')
+                    break
+            
+            
+
+    
+if not failed and not coherent:
+    print('YES')
+    
