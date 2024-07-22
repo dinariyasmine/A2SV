f96140f216c0b18f9ce0b3a13fcd6779d11cbbda
Yasmine Dinari <ly_dinari@esi.dz>
2024-07-22 20:57:48 +0100

    Created 'E - World is Mine' by A2SV Hub

---
+++ A2SV Remote G5 Contest #24 22-Jul-2024/E - World is Mine 183012.py
@@ -0,0 +1,39 @@
+# Problem: E - World is Mine - https://codeforces.com/gym/536373/problem/E
+
+
+import heapq
+from typing import Counter
+
+test = int(input())
+
+
+for _ in range(test):
+    n = int(input())
+    lst = list(map(int, input().split()))
+    
+    cpt = Counter(lst)
+    
+    cakes = []
+    
+    for t in cpt:
+        cakes.append((t,cpt[t]))
+    
+    cakes.sort()
+    
+    
+    total_Bob = 0
+    curr_Bob = []
+    
+    
+    
+    for i in range(len(cakes)):
+        bob_turn = total_Bob+cakes[i][1]
+        alice_turn = i-len(curr_Bob)
+        total_Bob += cakes[i][1]
+        heapq.heappush(curr_Bob, -cakes[i][1])
+        if bob_turn> alice_turn:
+            total_Bob += heapq.heappop(curr_Bob)
+            
+            
+            
+    print(len(cakes)-len(curr_Bob))
