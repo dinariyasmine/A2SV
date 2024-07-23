05d0ba6562982fa3fc53ef60c46c664bd9c01cbe
Yasmine Dinari <ly_dinari@esi.dz>
2024-07-23 19:41:43 +0100

    Created 'A - The Battle Of The Five Armies' by A2SV Hub

---
+++ A2SV Remote G5 Contest V 23-Jul-2024/A - The Battle Of The Five Armies 183289.py
@@ -0,0 +1,21 @@
+# Problem: A - The Battle Of The Five Armies - https://codeforces.com/gym/508510/problem/A
+
+cap = list(map(int, input().split()))
+nbr = list(map(int, input().split()))
+
+first = 0
+for i in range(0,3):
+    first += cap[i]*nbr[i]
+
+
+second = 0
+
+for i in range(3,5):
+    second += cap[i]*nbr[i]
+
+if first > second:
+    print('WIN')
+elif first < second:
+    print('LOSE')
+else:
+    print('DRAW')
