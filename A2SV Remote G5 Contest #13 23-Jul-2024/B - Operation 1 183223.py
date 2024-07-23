29afd1a05709388c2a74bd2263cce117655faa1b
Yasmine Dinari <ly_dinari@esi.dz>
2024-07-23 18:42:19 +0100

    Created 'B - Operation 1' by A2SV Hub

---
+++ A2SV Remote G5 Contest #13 23-Jul-2024/B - Operation 1 183223.py
@@ -0,0 +1,18 @@
+# Problem: B - Operation 1 - https://codeforces.com/gym/521860/problem/B
+
+a, m = map(int, input().split())
+
+op2 = a //2
+op1 = a %2
+ans = -1
+
+while op2 >= 0:
+    
+    if (op2 + op1) % m == 0:
+        ans = op2 + op1
+        break
+    
+    op2 -=1
+    op1 +=2
+
+print(ans)
