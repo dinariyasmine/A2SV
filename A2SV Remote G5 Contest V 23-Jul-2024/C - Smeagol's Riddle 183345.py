23f8d438b1bc5b599dc4c656abb84e5db7f934f1
Yasmine Dinari <ly_dinari@esi.dz>
2024-07-23 20:15:19 +0100

    Created 'C - Smeagol's Riddle' by A2SV Hub

---
+++ A2SV Remote G5 Contest V 23-Jul-2024/C - Smeagol's Riddle 183345.py
@@ -0,0 +1,11 @@
+# Problem: C - Smeagol's Riddle - https://codeforces.com/gym/508510/problem/C
+
+n = int(input())
+for _ in range(n):
+    s = input()
+
+    cpt = 0
+    for i in range(len(s)//2):
+        if s[i] != s[len(s)-i-1]:
+            cpt += 1
+    print(cpt)
