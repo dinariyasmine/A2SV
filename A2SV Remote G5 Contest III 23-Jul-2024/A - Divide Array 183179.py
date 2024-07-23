ee7d8b1bd16fdb404f37169751c3f233c74f294a
Yasmine Dinari <ly_dinari@esi.dz>
2024-07-23 16:21:01 +0100

    Created 'A - Divide Array' by A2SV Hub

---
+++ A2SV Remote G5 Contest III 23-Jul-2024/A - Divide Array 183179.py
@@ -0,0 +1,22 @@
+# Problem: A - Divide Array - https://codeforces.com/gym/505936/problem/A
+
+
+n = int(input())
+
+arr = sorted(list(map(int, input().split())))
+
+#print negative
+print(1,arr[0])
+arr.pop(0)
+
+#print(positive)
+if arr[-1] > 0:
+    print(1, arr[-1] )
+    arr.pop( -1 )
+else:
+    print(2,arr[0],arr[1])
+    arr.pop(0)
+    arr.pop(0)
+
+#print rest
+print(len(arr),*arr)
