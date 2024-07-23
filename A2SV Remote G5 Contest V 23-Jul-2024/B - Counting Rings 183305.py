97f19128b1b9b43c9699453d59139bf91b30abd5
Yasmine Dinari <ly_dinari@esi.dz>
2024-07-23 19:51:47 +0100

    Created 'B - Counting Rings' by A2SV Hub

---
+++ A2SV Remote G5 Contest V 23-Jul-2024/B - Counting Rings 183305.py
@@ -0,0 +1,15 @@
+# Problem: B - Counting Rings - https://codeforces.com/gym/508510/problem/B
+
+gandalf = input()
+curr = 0
+rings = 0
+for i in range(len(gandalf)):
+    if gandalf[i] == 'O':
+        
+        curr += 1
+        
+        rings = max (rings,curr)
+        
+    else:
+        curr = 0
+print(rings)
