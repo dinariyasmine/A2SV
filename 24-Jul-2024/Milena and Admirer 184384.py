7e42712939757ace1df4424385818391443bf216
Yasmine Dinari <ly_dinari@esi.dz>
2024-07-25 00:03:27 +0100

    Created 'Milena and Admirer' by A2SV Hub

---
+++ 24-Jul-2024/Milena and Admirer 184384.py
@@ -0,0 +1,16 @@
+# Problem: Milena and Admirer - https://codeforces.com/problemset/problem/1898/B
+
+import math
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    op = 0
+    for i in range(n - 1, 0, -1):
+        if a[i - 1] > a[i]:
+            k = math.ceil(a[i - 1] / a[i])
+            op += k - 1
+            a[i - 1] = math.floor(a[i - 1] / k)
+
+    print(op)
