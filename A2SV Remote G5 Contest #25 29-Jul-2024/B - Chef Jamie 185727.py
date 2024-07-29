bc43a85ea5555b1a99b867a60939b763e23826c3
Yasmine Dinari <ly_dinari@esi.dz>
2024-07-29 19:15:17 +0100

    Created 'B - Chef Jamie' by A2SV Hub

---
+++ A2SV Remote G5 Contest #25 29-Jul-2024/B - Chef Jamie 185727.py
@@ -0,0 +1,16 @@
+# Problem: B - Chef Jamie - https://codeforces.com/gym/537575/problem/B
+
+n = int(input())
+fruits = list(map(int, input().split()))
+fruits_sorted = sorted(fruits)
+
+
+cpt = 0
+
+for i in range(n):
+    cpt += fruits[i] != fruits_sorted[i]
+        
+sw = max(0,cpt - 1)
+        
+print(sw)
+
