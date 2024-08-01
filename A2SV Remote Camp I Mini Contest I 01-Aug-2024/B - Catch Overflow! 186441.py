03d2cfdf21dfb99308c694004100b2f0f313196d
Yasmine Dinari <ly_dinari@esi.dz>
2024-08-01 01:09:47 +0100

    Created 'B - Catch Overflow!' by A2SV Hub

---
+++ A2SV Remote Camp I Mini Contest I 01-Aug-2024/B - Catch Overflow! 186441.py
@@ -0,0 +1,29 @@
+# Problem: B - Catch Overflow! - https://codeforces.com/gym/520112/problem/B
+
+t = int(input())
+maxi = 2**32 - 1
+x = 0
+loops = [1]  
+
+for _ in range(t):
+    op = input().strip()
+
+    if op == 'add':
+        x += loops[-1]
+        if x > maxi:
+            print('OVERFLOW!!!')
+            exit()
+
+    elif op.startswith('for'):
+        v = int(op.split()[1])
+        
+        curr = loops[-1] * v
+        
+        if curr > maxi:
+            curr = maxi + 1  
+        loops.append(curr)
+        
+    elif op == 'end':
+        loops.pop()
+
+print(x)
