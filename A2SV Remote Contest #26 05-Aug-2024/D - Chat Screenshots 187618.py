b7ab8806b56dd54d3137db23a07fd657a5787dbf
Yasmine Dinari <ly_dinari@esi.dz>
2024-08-05 19:16:39 +0100

    Created 'D - Chat Screenshots' by A2SV Hub

---
+++ A2SV Remote Contest #26 05-Aug-2024/D - Chat Screenshots 187618.py
@@ -0,0 +1,31 @@
+# Problem: D - Chat Screenshots - https://codeforces.com/gym/540348/problem/D
+
+from collections import defaultdict, deque
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    graph = defaultdict(list)
+    prt = [0] * (n + 1)
+
+    for _ in range(k):
+        lst = list(map(int, input().split()))
+        for i in range(1, len(lst) - 1):
+            graph[lst[i]].append(lst[i + 1])
+            prt[lst[i + 1]] += 1
+
+    dq = deque()
+    for i in range(1, n + 1):
+        if prt[i] == 0:
+            dq.append(i)
+
+    answer = []
+    while dq:
+        node = dq.popleft()
+        answer.append(node)
+        for child in graph[node]:
+            prt[child] -= 1
+            if prt[child] == 0:
+                dq.append(child)
+
+    print('YES') if len(answer) == n else print('NO')
