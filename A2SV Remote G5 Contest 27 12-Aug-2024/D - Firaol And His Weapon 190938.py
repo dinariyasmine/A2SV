c472bba7b31466c63bbf3228fe3150d09dd834d8
Yasmine Dinari <ly_dinari@esi.dz>
2024-08-12 19:11:54 +0100

    Created 'D - Firaol And His Weapon' by A2SV Hub

---
+++ A2SV Remote G5 Contest 27 12-Aug-2024/D - Firaol And His Weapon 190938.py
@@ -0,0 +1,15 @@
+# Problem: D - Firaol And His Weapon - https://codeforces.com/gym/541399/problem/D
+
+from collections import Counter
+
+n = int(input())
+planets = list(map(int, input().split()))
+c = Counter(planets)
+max_mass = max(planets)
+dp = [0] * (max_mass + 1)
+dp[1] = c[1]
+
+for i in range(2, max_mass + 1):
+    dp[i] = max(dp[i-1], dp[i-2] + i * c[i])
+
+print(dp[max_mass])
