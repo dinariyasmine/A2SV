ba59d4f1c0b9edcc60b2507388a276e07487c8ed
Yasmine Dinari <ly_dinari@esi.dz>
2024-08-15 19:34:15 +0100

    Created 'Coin Change' by A2SV Hub

---
+++ 15-Aug-2024/Coin Change 191859.py
@@ -0,0 +1,16 @@
+# Problem: Coin Change - https://leetcode.com/problems/coin-change/
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [0] + ([float('inf')] * amount)
+
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if coin <= i:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+
+
+
+        if dp[-1] == inf:
+            return -1
+        return dp[-1]
