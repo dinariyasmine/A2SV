78f8cf9a59ba3d2bdd781cc5e58d4d3a11e38e51
Yasmine Dinari <ly_dinari@esi.dz>
2024-08-17 22:09:40 +0100

    Created 'C - Collecting Game' by A2SV Hub

---
+++ A2SV Remote G5 Contest 27 17-Aug-2024/C - Collecting Game 197567.py
@@ -0,0 +1,28 @@
+# Problem: C - Collecting Game - https://codeforces.com/gym/541399/problem/C
+
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().strip().split()))
+    a = sorted(arr, reverse = True)
+    
+    sf = a.copy()
+    
+    for i in range(n - 2, -1, -1):
+        sf[i] += sf[i + 1]
+        
+    ans = [0] * n
+    ans[0] = n - 1
+    
+    for i in range(1, n):
+        ans[i] = n - 1 - i
+        if sf[i] >= a[i - 1]:
+            ans[i] = ans[i - 1]
+    ans_dic = dict.fromkeys(a, 0)
+    for i in range(n):
+        ans_dic[a[i]] = ans[i]
+    for x in arr:
+        print(ans_dic[x], end = ' ')
+    print()
