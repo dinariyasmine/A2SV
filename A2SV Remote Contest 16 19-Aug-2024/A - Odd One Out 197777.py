b2b4b45f198ca28e0254712324c247fbee398097
Yasmine Dinari <ly_dinari@esi.dz>
2024-08-19 16:45:40 +0100

    Created 'A - Odd One Out' by A2SV Hub

---
+++ A2SV Remote Contest 16 19-Aug-2024/A - Odd One Out 197777.py
@@ -0,0 +1,16 @@
+# Problem: A - Odd One Out - https://codeforces.com/gym/525787/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    a, b, c = map(int, input().split())
+
+    if a == b:
+        print(c)
+        
+    elif a == c:
+        print(b)
+        
+    elif b == c:
+        print(a)
+       
