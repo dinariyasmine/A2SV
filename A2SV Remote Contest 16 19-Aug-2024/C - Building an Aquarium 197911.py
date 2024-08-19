c13e96f3a4e66a26bce21cc2d24202bef0b57cad
Yasmine Dinari <ly_dinari@esi.dz>
2024-08-19 17:49:19 +0100

    Created 'C - Building an Aquarium' by A2SV Hub

---
+++ A2SV Remote Contest 16 19-Aug-2024/C - Building an Aquarium 197911.py
@@ -0,0 +1,25 @@
+# Problem: C - Building an Aquarium - https://codeforces.com/gym/525787/problem/C
+
+t = int(input())
+
+for _ in range(t):
+    n, x = map(int, input().split())
+    a = list(map(int, input().split()))
+    
+    mini = 0
+    maxi = max(a) + x
+    
+    
+    while mini < maxi:
+        mid = mini + (maxi - mini + 1) // 2
+        curr = 0
+        
+        for i in range(n):
+            curr += max(mid - a[i], 0)
+            
+        if curr <= x:
+            mini = mid
+        else:
+            maxi = mid - 1
+    
+    print(mini)
