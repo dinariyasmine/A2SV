ab21c186dff3817f5bc968771a21131cdafddbdc
Yasmine Dinari <ly_dinari@esi.dz>
2024-08-20 20:56:31 +0100

    Created 'C - Hello World' by A2SV Hub

---
+++ A2SV Remote G5 Education Contest #28 20-Aug-2024/C - Hello World 198272.py
@@ -0,0 +1,19 @@
+# Problem: C - Hello World - https://codeforces.com/gym/543262/problem/C
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    arr = sorted(list(map(int, input().split())))
+    if arr[0] != 1:
+        print('NO')
+        continue
+    arr.pop(0)
+    cpt = 1
+    for i in arr:
+        if i > cpt:
+            print('NO')
+            break
+        cpt += i
+    else:
+        print('YES')
