a01552aad28f414159a39891efd3ed5ed5c659da
Yasmine Dinari <ly_dinari@esi.dz>
2024-08-26 20:32:17 +0100

    Created 'C - Tournament' by A2SV Hub

---
+++ A2SV Remote G5 Contest #29 26-Aug-2024/C - Tournament 204932.py
@@ -0,0 +1,29 @@
+# Problem: C - Tournament - https://codeforces.com/gym/544347/problem/C
+
+from math import floor
+
+def max_points(n, k, produce):
+    dp = [[0] * 14 for _ in range(n+1)]
+
+    for i in range(n - 1, -1, -1):
+        for x in range(14):
+            
+            curr = produce[i] // (2 ** x)
+            full = curr - k + dp[i + 1][x]
+            
+            if x + 1 < 14:
+                half = floor(curr / 2) + dp[i + 1][x + 1]
+            else:
+                half = 0  
+
+
+
+            dp[i][x] = max(full, half)
+
+    return dp[0][0]
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    produce = list(map(int, input().split()))
+    print(max_points(n, k, produce))
