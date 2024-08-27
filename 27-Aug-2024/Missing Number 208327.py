14d1b54639d3d3f2cc53a48130dcfb3c6d9e0b63
Yasmine Dinari <ly_dinari@esi.dz>
2024-08-27 18:19:40 +0100

    Created 'Missing Number' by A2SV Hub

---
+++ 27-Aug-2024/Missing Number 208327.py
@@ -0,0 +1,14 @@
+# Problem: Missing Number - https://leetcode.com/problems/missing-number/description/
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        ans = 0
+
+        for i in range(1, n + 1):
+            ans ^= i
+
+        for num in nums:
+            ans ^= num
+            
+        return ans
