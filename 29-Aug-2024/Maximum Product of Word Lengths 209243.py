f4f59c1054109ef437e6d897bfa14b64e4d0f81b
Yasmine Dinari <ly_dinari@esi.dz>
2024-08-29 21:48:17 +0100

    Created 'Maximum Product of Word Lengths' by A2SV Hub

---
+++ 29-Aug-2024/Maximum Product of Word Lengths 209243.py
@@ -0,0 +1,22 @@
+# Problem: Maximum Product of Word Lengths - https://leetcode.com/problems/maximum-product-of-word-lengths/
+
+class Solution:
+  def maxProduct(self, words: List[str]) -> int:
+    ans = 0
+
+    def getMask(word: str) -> int:
+      mask = 0
+      for ch in word:
+        mask |= 1 << ord(ch) - ord('a')
+      return mask
+
+    masks = [getMask(word) for word in words]
+    
+
+    for i in range(len(words)):
+      for j in range(i, len(words)):
+        # all positions end up = 0 when we do the "&" 101 & 010 = 000
+        if not (masks[i] & masks[j]):
+          ans = max(ans, len(words[i]) * len(words[j]))
+
+    return ans
