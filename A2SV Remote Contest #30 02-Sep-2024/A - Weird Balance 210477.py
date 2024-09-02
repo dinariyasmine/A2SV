eef88b85fdf6dc0bb04bf994b43f8a736fdc02e0
Yasmine Dinari <ly_dinari@esi.dz>
2024-09-02 19:44:25 +0100

    Created 'A - Weird Balance' by A2SV Hub

---
+++ A2SV Remote Contest #30 02-Sep-2024/A - Weird Balance 210477.py
@@ -0,0 +1,17 @@
+# Problem: A - Weird Balance - https://codeforces.com/gym/545837/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    if a.count(a[0]) == len(a):
+        print('NO')
+        continue
+        
+    a.sort(reverse=True)
+    
+    if a[0] == a[1]:
+        a[1], a[n - 1] = a[n - 1], a[1]
+    print('YES')
+    print(*a)
