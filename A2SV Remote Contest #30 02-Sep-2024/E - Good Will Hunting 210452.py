a458c67a6d0da15995635d48c9aabb957dd5801d
Yasmine Dinari <ly_dinari@esi.dz>
2024-09-02 19:00:06 +0100

    Created 'E - Good Will Hunting' by A2SV Hub

---
+++ A2SV Remote Contest #30 02-Sep-2024/E - Good Will Hunting 210452.py
@@ -0,0 +1,20 @@
+# Problem: E - Good Will Hunting - https://codeforces.com/gym/545837/problem/E
+
+l, r = map(int, input().split())
+
+a = l
+b = r
+
+for i in range((r - l).bit_length() - 1, -1, -1):
+    
+    if (a ^ b) & (1 << i) == 0:
+        
+        curr_a = a | (1 << i)  
+        curr_b = b & ~(1 << i)  
+        
+        if curr_a <= b and curr_a <= r:  
+            a = curr_a
+        if curr_b >= a and curr_b >= l:  
+            b = curr_b
+
+print(a ^ b)
