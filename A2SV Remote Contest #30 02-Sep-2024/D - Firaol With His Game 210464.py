f122c81187086d2edfb5e65de2da73a438988c72
Yasmine Dinari <ly_dinari@esi.dz>
2024-09-02 19:26:09 +0100

    Created 'D - Firaol With His Game' by A2SV Hub

---
+++ A2SV Remote Contest #30 02-Sep-2024/D - Firaol With His Game 210464.py
@@ -0,0 +1,24 @@
+# Problem: D - Firaol With His Game - https://codeforces.com/gym/545837/problem/D
+
+t = int(input())
+
+for _ in range(t):
+    n, s = map(int, input().split())
+    
+    a = list(map(int, input().split()))
+    
+    cpt = 0
+    maxi = 0
+    max_idx = 0
+
+    for i in range(n):
+        cpt += a[i]
+        
+        if a[i] > maxi:
+            maxi = a[i]
+            max_idx = i + 1
+        if cpt > s:
+            print(max_idx)
+            break
+    else:
+        print(0)
