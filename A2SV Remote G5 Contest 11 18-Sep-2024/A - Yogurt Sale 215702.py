f1b20328100d16d25102ea8bc599f14c04a15b2d
Yasmine Dinari <ly_dinari@esi.dz>
2024-09-18 01:11:03 +0100

    Created 'A - Yogurt Sale' by A2SV Hub

---
+++ A2SV Remote G5 Contest 11 18-Sep-2024/A - Yogurt Sale 215702.py
@@ -0,0 +1,11 @@
+# Problem: A - Yogurt Sale - https://codeforces.com/gym/518838/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    n, a, b = map(int, input().split())
+    
+    if ((n // 2) * b)  + ((n % 2) * a) < n * a:
+        print(((n // 2) * b)  + ((n % 2) * a))
+    else:
+        print(n * a)
