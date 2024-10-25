a40c7a4ddd51ef9eafaf147fdb95a6753ce8d2e7
Yasmine Dinari <ly_dinari@esi.dz>
2024-10-25 20:57:13 +0100

    Created 'Complicated GCD' by A2SV Hub

---
+++ 25-Oct-2024/Complicated GCD 222213.py
@@ -0,0 +1,10 @@
+# Problem: Complicated GCD - https://codeforces.com/contest/664/problem/A
+
+a, b = map(int, input().split())
+
+if a == b:
+    print(a)
+elif b - a == 1:
+    print(1)
+elif b - a > 1:
+    print(1)
