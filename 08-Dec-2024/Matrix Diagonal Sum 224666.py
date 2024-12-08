e392c667619b346379fa1e33bc963740d35aa17f
Yasmine Dinari <ly_dinari@esi.dz>
2024-12-08 21:08:06 +0100

    Created 'Matrix Diagonal Sum' by A2SV Hub

---
+++ 08-Dec-2024/Matrix Diagonal Sum 224666.py
@@ -0,0 +1,20 @@
+# Problem: Matrix Diagonal Sum - https://leetcode.com/problems/matrix-diagonal-sum/description/
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        
+        n = len(mat)
+        ans = 0
+        
+        for i in range(n):
+            ans += mat[i][i] + mat[i][n - i - 1]
+        if n % 2 == 1:
+            ans -= mat[n//2][n//2]
+
+            
+        return ans
+            
+            
+            
+
+
