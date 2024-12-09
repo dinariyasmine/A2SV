1ddf47ec49bc4fabdbf65203f6de477d9e152ba8
Yasmine Dinari <ly_dinari@esi.dz>
2024-12-09 19:04:32 +0100

    Created 'Best Time to Buy and Sell Stock II' by A2SV Hub

---
+++ 09-Dec-2024/Best Time to Buy and Sell Stock II 224689.py
@@ -0,0 +1,18 @@
+# Problem: Best Time to Buy and Sell Stock II - https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        if n == 0:
+            return 0
+        
+        dp = [[0] * 2 for _ in range(n)]
+        
+        dp[0][0] = 0 
+        dp[0][1] = -prices[0]  
+
+        for i in range(1, n):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+
+        return dp[n-1][0]
